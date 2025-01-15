0a1cb5bb9b45f65feda15f4e31669ed4cf7495cd
VijaaY17 <vijayrajendran03@gmail.com>
2025-01-15 14:28:05 +0530

    Test page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.use("/",(req,res) =>{
+  res.send("Test page")
+})
+
+app.use("/test",(req,res) =>{
+  res.send("Testing request handlers")
+})
+
+app.use((req,res) =>{
+  res.send("Hello")
+})
+
+
+
+
 app.listen(3000,() =>{
   console.log("Server is listening on port 3000")
 })
